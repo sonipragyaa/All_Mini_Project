8ff49a78f85a35db09f35e1463cecb1b9affc436
pragya <pragyasoni694@gmail.com>
2024-02-10 17:49:12 +0530

    second commit

--- CRUD/colorlib-regform-26/index.php
+++ CRUD/colorlib-regform-26/index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>CRUD OPERATION</title>
+		<title>CRUD OPERATIONssss</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<!-- LINEARICONS -->
